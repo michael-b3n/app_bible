a46ca56bc949e73807df98a9d757f971bcbb5653
MBK <mkaepsilon@outlook.com>
2025-09-16 22:54:24 +0200

    Bump version to 1.4

--- app_bible_assistant/src/version.hpp
+++ app_bible_assistant/src/version.hpp
@@ -15,8 +15,8 @@ namespace bible_assistant
 struct version final
 {
   static constexpr unsigned int major_number = 1;
-  static constexpr unsigned int minor_number = 3;
-  static constexpr auto version_string = std::string_view("1.3");
+  static constexpr unsigned int minor_number = 4;
+  static constexpr auto version_string = std::string_view("1.4");
   static constexpr auto commit_hash = std::string_view("");
   static constexpr auto commit_date = std::string_view("");
 };
